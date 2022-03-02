1d446ed0e1ba10b98fc1db89cc062a95dfc06c34
Camilo Hernandez <camehernandecas@unal.edu.co>
2022-03-02 07:28:01 -0500

    initial commit

---
+++ src/main/java/com/fetcher/api/gateway/config/RoutesConfig.java
@@ -0,0 +1,23 @@
+package com.fetcher.api.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fetcher.api.gateway.filters.AuthenticationFilter;
+
+@Configuration
+public class RoutesConfig {
+	
+		@Autowired
+		private AuthenticationFilter filter;
+
+		@Bean
+		public RouteLocator routes(RouteLocatorBuilder builder) {
+			return builder.routes().route("login", r -> r.path("/login/**").filters(f -> f.filter(filter)).uri("lb://Security"))
+					.route("books", r -> r.path("/books/**").filters(f -> f.filter(filter)).uri("lb://books")).build();
+		}
+
+}
